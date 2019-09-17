ae141012b900a9e04bd4979580c1468361ecc369
shailesh kodulkar <shailesh.kodulkar@reisystems.in>
2019-09-17 11:57:08 +0530

    Adding new addition method in the MainClass file

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,2 +1,10 @@
+import java.util.*;
 public class MainClass {
+   static int addition(int a,int b)
+    {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        System.out.println(addition(10,15));
+    }
 }
